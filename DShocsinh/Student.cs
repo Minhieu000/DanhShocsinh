9eacec834b46c4efb9b9c8911a42dcc2253c7f9f
THIS PC <THIS PC@LAPTOP-13UV3BRD>
2024-11-20 01:30:57 +0700

    Add project files.

---
+++ DShocsinh/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace DanhShocsinh
+{
+    internal class Student
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public int Age { get; internal set; }
+    }
+}
